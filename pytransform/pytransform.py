a86b3610aebe16cc7df55783b7b603abd6207c0e
cpyberry <cpyberry222@gmail.com>
2021-04-11 15:09:45 +0900

    feat: add a function transform_dictionary
    This function is called recursively, so it can also be applied to nested dictionary

--- pytransform/pytransform.py
+++ pytransform/pytransform.py
@@ -33,3 +33,36 @@ def transform_list(origin: list, operation=_default_transform) -> list:
 		result.append(transformed_element)
 
 	return result
+
+
+def transform_dictionary(origin: dict, operation_key=_default_transform, operation_value=_default_transform) -> dict:
+	"""Apply the specified function to each key and value of dictionary
+
+	This function is called recursively, so it can also be applied to nested dictionary
+
+	Args:
+		origin (dict): origin container
+		operation_key (function, optional): unction you want to apply to each key. Defaults to _default_transform.
+		operation_value (function, optional): unction you want to apply to each value. Defaults to _default_transform.
+
+	Returns:
+		dict: dictionary to which the specified function is applied
+	"""
+	result = {}
+
+	for key in origin:
+		value = origin[key]
+
+		if type(key) == dict:
+			transformed_key = transform_dictionary(key, operation_key, operation_value)
+		else:
+			transformed_key = operation_key(key, origin, value)
+
+		if type(value) == dict:
+			transformed_value = transform_dictionary(value, operation_key, operation_value)
+		else:
+			transformed_value = operation_value(value, origin, key)
+
+		result.update({transformed_key: transformed_value})
+
+	return result
