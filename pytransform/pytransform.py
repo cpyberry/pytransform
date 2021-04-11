ef248b1945b79acddf5a1d416a7fdeb0ca09e800
cpyberry <cpyberry222@gmail.com>
2021-04-11 14:47:06 +0900

    feat: add a function transform_list
    This function is called recursively, so it can also be applied to nested elements

--- pytransform/pytransform.py
+++ pytransform/pytransform.py
@@ -9,3 +9,27 @@ def _default_transform(element, origin, *args, **kwargs):
 		origin: origin container
 	"""
 	return element
+
+
+def transform_list(origin: list, operation=_default_transform) -> list:
+	"""Apply the specified function to each element of list
+
+	This function is called recursively, so it can also be applied to nested elements
+
+	Args:
+		origin (list): origin container
+		operation (function, optional): function you want to apply to each element. Defaults to _default_transform.
+
+	Returns:
+		list: list to which the specified function is applied
+	"""
+	result = []
+
+	for element in origin:
+		if type(element) == list:
+			transformed_element = transform_list(element, operation)
+		else:
+			transformed_element = operation(element, origin)
+		result.append(transformed_element)
+
+	return result
