3f15ecd6ff3be4c9eec129e9f5e1c25b8b6267b1
cpyberry <cpyberry222@gmail.com>
2021-09-14 00:51:07 +0900

    feat: add information to __init__.py
    __title__, __description__ and __python_requires__

--- pytransform/__init__.py
+++ pytransform/__init__.py
@@ -27,9 +27,12 @@ from .pytransform import (
 )
 
 
+__title__ = "cpyberry-pytransform"
+__description__ = "A library that applies arbitrary functions to each element of list type and dict type objects"
 __copyright__ = "Copyright 2021 cpyberry"
 __url__ = "https://github.com/cpyberry/pytransform"
 __license__ = "Apache-2.0 License"
 __version__ = "1.0.1"
 __author__ = "cpyberry"
 __author_email__ = "cpyberry222@gmail.com"
+__python_requires__ = ">=3.5"
